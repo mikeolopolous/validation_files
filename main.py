91bcddfdb52c8a2923c7346e002d502e707cb824
Miguel Lopez <ing.miguell@gmail.com>
2023-04-15 12:24:45 -0600

    agrego labels para ingreso de datos del archivo

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 import os
 import shutil
 from pathlib import Path
+import customtkinter as ctk
 
-PREFIX = "m"
-PATENTE = "3977"
 PATH_ORIGEN = "/Users/miguel/Downloads/"
 PATH_DESTINO = "/Users/miguel/Documents/"
-
-consecutivo = "001"
-juliano = "err"
+PREFIX = "m"
+PATENTE = "3977"
 
 remote_path = Path(PATH_ORIGEN)
 os.chdir(remote_path)
-# base_path = os.getcwd()
-# path = Path(base_path)
 
-# for p in path.glob(f"{PREFIX}{PATENTE}{consecutivo}.{juliano}"):
-#     print(p)
+root = ctk.CTk()
+root.geometry("400x500")
+root.title("Duplicador de archivos SAAIM3")
 
+title_label = ctk.CTkLabel(root, text="Copia de SAAIM3",
+                           font=ctk.CTkFont(size=30, weight="bold"))
+title_label.pack(padx=5, pady=(10, 20))
+
+frame = ctk.CTkFrame(root)
+frame.pack(fill="x", padx=25)
+
+consecutivo_frame = ctk.CTkFrame(frame)
+consecutivo_frame.pack(padx=25, pady=(10, 5), fill="both")
+consecutivo_label = ctk.CTkLabel(consecutivo_frame, text="Consecutivo de archivo:",
+                                 font=ctk.CTkFont(weight="bold"))
+consecutivo_label.pack()
+
+juliano_frame = ctk.CTkFrame(frame)
+juliano_frame.pack(padx=25, pady=(10, 5), fill="both")
+juliano_label = ctk.CTkLabel(juliano_frame, text="Día juliano:",
+                                 font=ctk.CTkFont(weight="bold"))
+juliano_label.pack()
+
+custom_name_frame = ctk.CTkFrame(frame)
+custom_name_frame.pack(padx=25, pady=(10, 5), fill="both")
+custom_name_label = ctk.CTkLabel(custom_name_frame, text="Extensión:",
+                                 font=ctk.CTkFont(weight="bold"))
+custom_name_label.pack()
+
+consecutivo = "001"
+juliano = "err"
 nombre_original = f"{PREFIX}{PATENTE}{consecutivo}.{juliano}"
 
-archivos = os.listdir()
-for archivo in archivos:
-    if archivo == nombre_original:
-        shutil.copy(src=PATH_ORIGEN + nombre_original, dst=PATH_DESTINO)
-        break
+# archivos = os.listdir()
+# for archivo in archivos:
+#     if archivo == nombre_original:
+#         shutil.copy(src=PATH_ORIGEN + nombre_original, dst=PATH_DESTINO)
+#         break
+
+root.mainloop()
