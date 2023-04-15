6a755432ad3b66886b6e8c48ca2b63d8f28b3f0c
Miguel Lopez <ing.miguell@gmail.com>
2023-04-15 10:03:16 -0600

    agrego la busqueda del archivo en origen y realizo una copia a carpeta destino

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import os
+import shutil
 from pathlib import Path
 
-PATENTE = "m3977"
-
-remote_path = Path("/Users/miguel/Downloads")
-os.chdir(remote_path)
-base_path = os.getcwd()
-path = Path(base_path)
+PREFIX = "m"
+PATENTE = "3977"
+PATH_ORIGEN = "/Users/miguel/Downloads/"
+PATH_DESTINO = "/Users/miguel/Documents/"
 
 consecutivo = "001"
 juliano = "err"
-for p in path.glob(f"{PATENTE}{consecutivo}.{juliano}"):
-    print(p)
+
+remote_path = Path(PATH_ORIGEN)
+os.chdir(remote_path)
+# base_path = os.getcwd()
+# path = Path(base_path)
+
+# for p in path.glob(f"{PREFIX}{PATENTE}{consecutivo}.{juliano}"):
+#     print(p)
+
+nombre_original = f"{PREFIX}{PATENTE}{consecutivo}.{juliano}"
+
+archivos = os.listdir()
+for archivo in archivos:
+    if archivo == nombre_original:
+        shutil.copy(src=PATH_ORIGEN + nombre_original, dst=PATH_DESTINO)
+        break
