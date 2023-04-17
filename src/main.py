ac34443c62f87c18ee0b867e1d6123f1dd912083
Miguel Lopez <ing.miguell@gmail.com>
2023-04-17 15:07:55 -0600

    agrego mensajes al usuario

--- src/main.py
+++ src/main.py
@@ -5,6 +5,10 @@ import customtkinter as ctk
 
 BLUE_COLOR = "#227C9D"
 GREEN_COLOR = "#17C3B2"
+YELLOW_COLOR = "#FFCB77"
+RED_COLOR = "#FE6D73"
+
+# PATH_REMOTO = "\\192.168.11.99\LWOperacionMZO\Enviar"
 
 PATH_ORIGEN = "/Users/miguel/Downloads/"
 PATH_DESTINO = "/Users/miguel/Documents/"
@@ -21,15 +25,32 @@ def make_copy():
     nombre_con_extension = nombre_original + "." + extension
 
     archivos = os.listdir()
-    for archivo in archivos:
-        if archivo == nombre_original:
-            if extension != "":
-                shutil.copy(src=PATH_ORIGEN + nombre_original, dst=PATH_DESTINO)
-                os.rename(PATH_DESTINO + nombre_original, PATH_DESTINO + nombre_con_extension)
-            else:
-                shutil.copy(src=PATH_ORIGEN + nombre_original, dst=PATH_DESTINO)
-            
-            break
+    if consecutivo == "" or juliano == "":
+        message_text.set("Revisa el consecutivo o juliano")
+        message_label = ctk.CTkLabel(root,
+                                     textvariable=message_text,
+                                     text_color=YELLOW_COLOR,
+                                     font=ctk.CTkFont(size=20, weight="bold"))
+        message_label.pack(padx=5, pady=(10, 20))
+    elif nombre_original in archivos:
+        if extension != "":
+            shutil.copy(src=PATH_ORIGEN + nombre_original, dst=PATH_DESTINO)
+            os.rename(PATH_DESTINO + nombre_original, PATH_DESTINO + nombre_con_extension)
+        else:
+            shutil.copy(src=PATH_ORIGEN + nombre_original, dst=PATH_DESTINO)
+        
+        message_text.set("¡Copia realizada!")
+        message_label = ctk.CTkLabel(root,
+                                     textvariable=message_text,
+                                     text_color=GREEN_COLOR,
+                                     font=ctk.CTkFont(size=30, weight="bold"))
+        message_label.pack(padx=5, pady=(10, 20))
+    else:
+        message_text.set("¡No se encontró el archivo!")
+        message_label = ctk.CTkLabel(root,
+                                     textvariable=message_text,
+                                     text_color=RED_COLOR,
+                                     font=ctk.CTkFont(size=20, weight="bold"))
 
 
 remote_path = Path(PATH_ORIGEN)
@@ -39,6 +60,8 @@ root = ctk.CTk()
 root.geometry("400x450")
 root.resizable(False, False)
 root.title("Duplicador de archivos SAAIM3")
+
+message_text = ctk.StringVar()
 
 title_label = ctk.CTkLabel(root,
                            text="Copia de SAAIM3",
