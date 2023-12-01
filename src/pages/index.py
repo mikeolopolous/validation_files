f41d5f6db29a8b0c0656589cf55f7ec44f5a3cab
Miguel Lopez <ing.miguellv@gmail.com>
2023-12-01 16:10:23 -0600

    main view

--- src/pages/index.py
+++ src/pages/index.py
@@ -4,14 +4,20 @@ import flet as ft
 def index_view():
     return ft.View(
         route='/',
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        vertical_alignment=ft.CrossAxisAlignment.CENTER,
         appbar=ft.AppBar(
             leading=ft.Icon(
                 name=ft.icons.AUTO_AWESOME_MOTION,
                 color=ft.colors.BLUE_500,
                 size=36,
             ),
-            title=ft.Text(value='Menú principal', color=ft.colors.BLUE_GREY_500),
-            bgcolor=ft.colors.SURFACE_VARIANT,
+            title=ft.Text(
+                value='Inicio',
+                color=ft.colors.BLUE_300,
+                size=35
+            ),
+            bgcolor=ft.colors.GREY_900,
             actions=[
                 ft.IconButton(
                     icon=ft.icons.ARROW_CIRCLE_UP,
@@ -29,4 +35,24 @@ def index_view():
                 ),
             ],
         ),
+        controls=[
+            ft.Text(value='Copia de archivos SAAIM3', size=35),
+            ft.Container(
+                width=550,
+                height=620,
+                alignment=ft.alignment.center,
+                bgcolor=ft.colors.GREY_900,
+                border_radius=ft.border_radius.all(5),
+                content=ft.Row(
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    controls=[
+                        ft.Image(
+                            src='../../imgs/logo_lw.png',
+                            width=400,
+                            height=400,
+                        ),
+                    ]
+                )
+            )
+        ]
     )
